8644d0e507ff542389dcee4cbfc5479e5f81d09d
Vitaly Greck <vintozver@ya.ru>
2020-04-15 13:41:54 -0700

    Fix import sorting

--- ssdp/__init__.py
+++ ssdp/__init__.py
@@ -1,10 +1,10 @@
 """Python library for Simple Service Discovery Protocol (SSDP)."""
 
+import ssdp.asyncio as asyncio
 import ssdp.entity as entity
 import ssdp.network as network
-import ssdp.asyncio as asyncio
 
-from ssdp.entity import *
 from ssdp.asyncio import *
+from ssdp.entity import *
 
 __all__ = entity.__all__ + asyncio.__all__
